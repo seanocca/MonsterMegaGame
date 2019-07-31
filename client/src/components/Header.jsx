62e8aa4f200df21d52a0c575237ba315ea12b52b
seanocca <32288543+seanocca@users.noreply.github.com>
2019-07-31 12:39:28 +1000

    Correctly Formatted Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,44 @@
 import React, { Component } from "react";
-import { Navbar, Form, Button, Nav, FormControl } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: null };
+
+  }
+
+  getUser(){
+    //this.user = //Get user from Session
+    if (this.state.user !== null){
+      return (
+        <NavDropdown title="User###" id="basic-nav-dropdown" className="justify-content-right">
+          <NavDropdown.Item href="#login">User Details</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      );
+    } else {
+      return (
+      <Navbar.Text className="justify-content-right">
+        Signed in as: <a href="#login">Mark Otto</a>
+      </Navbar.Text>
+      );
+    }
+  }
+
   render() {
     return (
       <Navbar bg="light" variant="light" fixed="top">
-        <Navbar.Brand href="#home">Half Monster Games</Navbar.Brand>
-        <Nav>
+        <Navbar.Brand href="#home" className="mr-auto">Half Monster Games</Navbar.Brand>
+        <Nav className="justify-content-center mr-auto" defaultActiveKey="#home">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" />
-          <Button className="" variant="outline-primary">
-            Search
-          </Button>
-        </Form>
+        {this.getUser()}
       </Navbar>
     );
   }
