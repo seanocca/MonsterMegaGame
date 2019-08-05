4e97d20c7566b7dee570e473a75793c57b81e229
death667b <idaniel@spinnys.com>
2019-08-05 13:49:02 +1000

    Update index.js mostly testing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const processError = require('../utils/processError');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send('Server up!');
+});
+
+router.get('/test', async (req, res, next) => {
+  const getParam = { TableName: 'icecreams' };
+
+  req.app.get('dynamoDb').scan(getParam).promise().then(response => {
+    res.status(200).send(response.Items);
+  }).catch(error => processError(error, res));
+});
+
+router.post('/test', async (req, res, next) => {
+  const { name, id } = req.body;
+
+  const params = {  
+    TableName: 'icecreams',  
+    Item: {
+        icecreamid: id,
+        name: name,
+        hhh: 'kkk'
+    } 
+  };
+
+  req.app.get('dynamoDb').put(params).promise().then(response => {
+    res.status(200).send(`Hi ${name}, you are ID ${id}.`);
+  }).catch(error => processError(error, res));
 });
 
 module.exports = router;
