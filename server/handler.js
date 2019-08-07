a3011d6e98d993a0e60d518b81d46593b7895340
death667b <idaniel@spinnys.com>
2019-08-07 13:25:15 +1000

    esLint bashing

--- server/handler.js
+++ server/handler.js
@@ -1,14 +1,14 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
-  };
-};
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line import/prefer-default-export
+export const hello = async (event, context) => ({
+  statusCode: 200,
+  body: JSON.stringify({
+    // eslint-disable-next-line no-use-before-define
+    message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!' }))}`,
+  }),
+});
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+// eslint-disable-next-line no-unused-vars
+const message = ({ time, ...rest }) => new Promise((resolve, reject) => setTimeout(() => {
+  resolve(`${rest.copy} (with a delay)`);
+}, time * 1000));
