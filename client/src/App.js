6b8be2dc93abec941ecc22cf18bbf5460ae05b6a
squed <squed@squeds-iMac.lan>
2019-08-09 18:58:20 +1000

    make basis AdminLogin page

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
+import AdminLogin from './pages/AdminLogin';
 import Account from './pages/Account';
 import Factions from './pages/Factions';
 import Bestiary from './pages/Bestiary';
@@ -18,7 +19,6 @@ import Lore from './pages/Lore';
 import GameRules from './pages/GameRules';
 
 import NotFound from './pages/NotFound';
-
 
 const App = () => (
   <Fragment>
@@ -28,6 +28,7 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Redirect from="/home" to="/" />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/admin" component={AdminLogin} />
         <Route exact path="/account" component={Account} />
         <Route exact path="/factions" component={Factions} />
         <Route exact path="/bestiary" component={Bestiary} />
