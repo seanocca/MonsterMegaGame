71c0696c212c2c052db0e007a7bbefcf3767b144
squed <squed@squeds-MacBook-Pro.local>
2019-08-11 15:35:43 +1000

    setup basis admin dashboard tabs

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { Container, Nav } from 'react-bootstrap';
 
 class Dashboard extends Component {
   render() {
-    return <h1>Admin page</h1>;
+    return (
+      <Container>
+        <Nav variant="tabs" defaultActiveKey="admin">
+          <Nav.Item>
+            <Nav.Link eventKey="admin">Admin</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="user-data">User Data</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="game-data">Game Data</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="beasts">Beasts</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="factions">Factions</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    );
   }
 }
 
