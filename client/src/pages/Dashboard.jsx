afae445de7743dd69b0cb6fe8cfd93c9dae12dc4
squed <squed@squeds-iMac.lan>
2019-08-11 21:35:59 +1000

    change dashboard menu to hrefs.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,19 +7,19 @@ class Dashboard extends Component {
       <Container>
         <Nav variant="tabs" defaultActiveKey="admin">
           <Nav.Item>
-            <Nav.Link eventKey="admin">Admin</Nav.Link>
+            <Nav.Link href="/dashboard/admin">Admin</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="user-data">User Data</Nav.Link>
+            <Nav.Link href="/dashboard/users">Users</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="game-data">Game Data</Nav.Link>
+            <Nav.Link href="/dashboard/game">Game Data</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="beasts">Beasts</Nav.Link>
+            <Nav.Link href="/dashboard/beasts">Beasts</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="factions">Factions</Nav.Link>
+            <Nav.Link href="/dashboard/factions">Factions</Nav.Link>
           </Nav.Item>
         </Nav>
       </Container>
