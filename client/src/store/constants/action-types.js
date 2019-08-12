c37a5609f203a710aafdb2d17af3b6c799fc5f76
death667b <idaniel@spinnys.com>
2019-08-12 15:43:47 +1000

    Added new constant PROCESS_USER_AUTHENTICATION

--- client/src/store/constants/action-types.js
+++ client/src/store/constants/action-types.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export const USER_AUTHENTICATION = 'USER_AUTHENTICATION';
+export const PROCESS_USER_AUTHENTICATION = 'PROCESS_USER_AUTHENTICATION';
