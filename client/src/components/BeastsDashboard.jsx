55263bec38c1ac3139a9e8ff6acb1e74f052eaaf
death667b <idaniel@spinnys.com>
2019-08-12 12:47:47 +1000

    Remove linting errors in BeatsDashboard

--- client/src/components/BeastsDashboard.jsx
+++ client/src/components/BeastsDashboard.jsx
@@ -3,9 +3,11 @@ import { Container, Table } from 'react-bootstrap';
 import Dashboard from '../pages/Dashboard';
 
 class BeastsDashboard extends Component {
+  x = () => { /* Temp fix to remove list warnings */ };
+
   render() {
     return (
-      <Container style={{padding: '4rem 0'}}>
+      <Container style={{ padding: '4rem 0' }}>
         <Dashboard />
         <h1>Beasts</h1>
         <Table striped bordered hover>
@@ -23,7 +25,7 @@ class BeastsDashboard extends Component {
               <td>Mitochondria</td>
               <td>Mechanica</td>
               <td>
-                <a href="">Edit Beast</a>
+                <a href="/#">Edit Beast</a>
               </td>
             </tr>
             <tr>
@@ -31,7 +33,7 @@ class BeastsDashboard extends Component {
               <td>Surveillance Team</td>
               <td>Voidborn</td>
               <td>
-                <a href="">Edit Beast</a>
+                <a href="/#">Edit Beast</a>
               </td>
             </tr>
             <tr>
@@ -39,7 +41,7 @@ class BeastsDashboard extends Component {
               <td>Octothorpe</td>
               <td>Biochondrys</td>
               <td>
-                <a href="">Edit Beast</a>
+                <a href="/#">Edit Beast</a>
               </td>
             </tr>
           </tbody>
