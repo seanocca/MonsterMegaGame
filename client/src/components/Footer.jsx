6ff696679bd1533e7570fe7b084a2fd68cee3293
seanocca <32288543+seanocca@users.noreply.github.com>
2019-08-16 18:45:01 +1000

    Footer Styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,30 +7,23 @@ import FBLogo from '../images/facebook.png';
 import InstaLogo from '../images/instagram.png';
 import YTLogo from '../images/youtube.png';
 import PatLogo from '../images/patreon.png';
-
-const stickyFooter = {
-  position: 'absolute',
-  width: '100%',
-  bottom: 0,
-  left: 0,
-  fontSize: '0.75rem',
-};
+import './Footer.css'
 
 class Footer extends Component {
   render() {
     return (
-      <Card bg="light" style={stickyFooter} className="pagination-centered">
+      <Card className="pagination-centered custom-footer">
         <Row
           className="justify-content-md-center"
           style={{ marginLeft: '0', marginRight: '0' }}
         >
-          <Col md="auto">
+          <Col md="auto" sm={6} xs={12}>
             <Card.Text className="text-center">Brisbane, Australia</Card.Text>
             <Card.Text className="text-center">
               halfhuman@halfmonstergames.com
             </Card.Text>
           </Col>
-          <Col md="auto">
+          <Col md="auto" sm={6} xs={12}>
             <Card.Text className="text-center">Contact Us!</Card.Text>
             <Card.Text className="text-center">
               <a href="https://facebook.com/HalfMonsterGames">
@@ -39,11 +32,7 @@ class Footer extends Component {
                   thumbnail
                   width="40"
                   height="40"
-                  style={{
-                    padding: '0px 5px',
-                    border: 'none',
-                    backgroundColor: '#fff0',
-                  }}
+                  className="custom-footer-socials"
                 />
               </a>
               <a href="https://www.instagram.com/halfmonstergames/">
@@ -52,24 +41,15 @@ class Footer extends Component {
                   thumbnail
                   width="40"
                   height="40"
-                  style={{
-                    padding: '0px 5px',
-                    border: 'none',
-                    backgroundColor: '#fff0',
-                  }}
+                  className="custom-footer-socials"
                 />
               </a>
               <a href="https://www.youtube.com/channel/UCPm1k8Gy3otn7fUZGaFFy5Q">
                 <Image
                   src={YTLogo}
                   thumbnail
-                  width="40"
                   height="40"
-                  style={{
-                    padding: '0px 5px',
-                    border: 'none',
-                    backgroundColor: '#fff0',
-                  }}
+                  className="custom-footer-socials"
                 />
               </a>
               <a href="https://www.patreon.com/jackfordmorgan">
@@ -78,20 +58,13 @@ class Footer extends Component {
                   thumbnail
                   width="40"
                   height="40"
-                  style={{
-                    padding: '0px 5px',
-                    border: 'none',
-                    backgroundColor: '#fff0',
-                  }}
+                  className="custom-footer-socials"
                 />
               </a>
             </Card.Text>
           </Col>
         </Row>
-        <Card.Footer
-          className="text-muted text-center"
-          style={{ fontSize: '0.6rem', padding: '0.5rem 1.25rem' }}
-        >
+        <Card.Footer className="text-muted text-center custom-footer-card">
           &copy; 2019 by Half-Monster Games
         </Card.Footer>
       </Card>
