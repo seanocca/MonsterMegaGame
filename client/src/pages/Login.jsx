cbea1cf1d71739b1fa00b7303d62747a8f052136
death667b <idaniel@spinnys.com>
2019-08-18 00:23:24 +1000

    Fix the missing onClick error

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Container, Card, Row, Col, Tabs, Tab } from 'react-bootstrap';
 import ConfirmationForm from '../components/LoginForms/ConfirmationForm';
@@ -11,47 +11,51 @@ const mapStateToProps = state => {
   };
 };
 
-const Login = ({ user }) => (
-  <Container style={{ padding: '4rem 0' }}>
-    <Row>
-      <Col md={{ span: 6, offset: 3 }}>
-        {user === null
-          ? (
-            <Fragment>
-              <Tabs defaultActiveKey="login" id="loginSignupConfirmationAAA">
-                <Tab eventKey="login" title="Login">
-                  <Card>
-                    <Card.Body>
-                      <LoginForm />
-                    </Card.Body>
-                  </Card>
-                </Tab>
-                <Tab eventKey="signUp" title="Sign Up">
-                  <Card>
-                    <Card.Body>
-                      <SignUpForm />
-                    </Card.Body>
-                  </Card>
-                </Tab>
-              </Tabs>
-            </Fragment>
-          )
-          : (
-            <Fragment>
-              <Tabs activeKey="confirmation" id="loginSignupConfirmationBBB">
-                <Tab eventKey="confirmation" title="Confirm Email">
-                  <Card>
-                    <Card.Body>
-                      <ConfirmationForm />
-                    </Card.Body>
-                  </Card>
-                </Tab>
-              </Tabs>
-            </Fragment>
-          )}
-      </Col>
-    </Row>
-  </Container>
-);
+const Login = ({ user }) => {
+  const [key, setKey] = useState('confirmation');
+
+  return (
+    <Container style={{ padding: '4rem 0' }}>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          {user === null
+            ? (
+              <Fragment>
+                <Tabs defaultActiveKey="login" id="loginSignupConfirmationAAA">
+                  <Tab eventKey="login" title="Login">
+                    <Card>
+                      <Card.Body>
+                        <LoginForm />
+                      </Card.Body>
+                    </Card>
+                  </Tab>
+                  <Tab eventKey="signUp" title="Sign Up">
+                    <Card>
+                      <Card.Body>
+                        <SignUpForm />
+                      </Card.Body>
+                    </Card>
+                  </Tab>
+                </Tabs>
+              </Fragment>
+            )
+            : (
+              <Fragment>
+                <Tabs activeKey={key} onSelect={k => setKey(k)} id="loginSignupConfirmationBBB">
+                  <Tab eventKey="confirmation" title="Confirm Email">
+                    <Card>
+                      <Card.Body>
+                        <ConfirmationForm />
+                      </Card.Body>
+                    </Card>
+                  </Tab>
+                </Tabs>
+              </Fragment>
+            )}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default connect(mapStateToProps)(Login);
