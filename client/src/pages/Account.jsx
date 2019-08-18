43b9dcb354a779bb678c3b83302f3de78d4d4bc5
death667b <idaniel@spinnys.com>
2019-08-18 21:42:03 +1000

    Make code sexy

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,24 +1,20 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-import AccEditForm from "../components/AccountEditForm";
-import FactionEdit from "./../components/FactionEdit";
+import AccEditForm from '../components/AccountEditForm';
+import FactionEdit from '../components/FactionEdit';
 
-class Account extends Component {
-  render() {
-    return (
-      <Container style={{padding: '4rem 0'}}>
-        <Row>
-          <Col md={6}>
-            <AccEditForm />
-          </Col>
-          <Col md={6}>
-            <FactionEdit />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+const Account = () => (
+  <Container style={{ padding: '4rem 0' }}>
+    <Row>
+      <Col md={6}>
+        <AccEditForm />
+      </Col>
+      <Col md={6}>
+        <FactionEdit />
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Account;
