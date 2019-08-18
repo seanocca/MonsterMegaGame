d0a7e5140aebb679034ffe30b99e47596e2bda4b
death667b <idaniel@spinnys.com>
2019-08-18 22:20:43 +1000

    Wired AccountForm to redux

--- client/src/components/AccountEditForm.jsx
+++ client/src/components/AccountEditForm.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import {
   Form, Col, Card, Button,
 } from 'react-bootstrap';
 
-const AccEditForm = () => {
-  console.log('blah');
+const seedState = {
+  email: '',
+  password: '',
+  confirm: '',
+  firstName: '',
+  lastName: '',
+  address: '',
+  city: '',
+  postcode: '',
+  state: '',
+  cognitoID: '',
+};
+
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+const AccEditForm = ({ user }) => {
+  const [formValues, setFormValues] = useState(seedState);
+
+  useEffect(() => {
+    if (user !== null) setFormValues(user);
+  }, [user]);
+
+  const handleChange = (event) => {
+    event.persist();
+    setFormValues(values => ({ ...values, [event.target.id]: event.target.value }));
+  };
 
   return (
     <Card>
@@ -12,28 +39,46 @@ const AccEditForm = () => {
       <Form style={{ padding: '1rem' }}>
         <Card.Body inline="true">
           <Form.Row>
-            <Form.Group as={Col} controlId="formFirstName">
+            <Form.Group as={Col} controlId="firstName">
               <Form.Label>First Name</Form.Label>
-              <Form.Control placeholder="FName" />
+              <Form.Control
+                placeholder="FName"
+                value={formValues.firstName}
+                onChange={handleChange}
+              />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formLastName">
+            <Form.Group as={Col} controlId="lastName">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control placeholder="LName" />
+              <Form.Control
+                placeholder="LName"
+                value={formValues.lastName}
+                onChange={handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Row>
-            <Form.Group as={Col} controlId="formUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control placeholder="current@email.com" />
+            <Form.Group as={Col} controlId="cognitoID">
+              <Form.Label>Database ID</Form.Label>
+              <Form.Control
+                placeholder="abc123"
+                value={formValues.cognitoID}
+                onChange={handleChange}
+                disabled
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Row>
-            <Form.Group as={Col} controlId="formEmail">
+            <Form.Group as={Col} controlId="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="current@email.com" />
+              <Form.Control
+                type="email"
+                placeholder="current@email.com"
+                value={formValues.email}
+                onChange={handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
@@ -49,33 +94,55 @@ const AccEditForm = () => {
             </Form.Group>
           </Form.Row>
 
-          <Form.Group controlId="formGridAddress">
+          <Form.Group controlId="address">
             <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" />
+            <Form.Control
+              placeholder="1234 Main St"
+              value={formValues.address}
+              onChange={handleChange}
+            />
           </Form.Group>
 
           <Form.Row>
-            <Form.Group as={Col} controlId="formGridCity">
+            <Form.Group as={Col} controlId="city">
               <Form.Label>City</Form.Label>
-              <Form.Control />
+              <Form.Control
+                placeholder="San Diego"
+                value={formValues.city}
+                onChange={handleChange}
+              />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridState">
+            <Form.Group as={Col} controlId="state">
               <Form.Label>State</Form.Label>
-              <Form.Control as="select">
-                <option>Choose...</option>
-                <option>...</option>
+              <Form.Control
+                as="select"
+                value={formValues.state}
+                onChange={handleChange}
+              >
+                <option value="">Choose...</option>
+                <option value="QLD">QLD</option>
+                <option value="NSW">NSW</option>
+                <option value="VIC">VIC</option>
+                <option value="NT">NT</option>
+                <option value="SA">SA</option>
+                <option value="WA">WA</option>
+                <option value="ACT">ACT</option>
               </Form.Control>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridZip">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control />
+            <Form.Group as={Col} controlId="postcode">
+              <Form.Label>Post Code</Form.Label>
+              <Form.Control
+                placeholder="1234"
+                value={formValues.postcode}
+                onChange={handleChange}
+              />
             </Form.Group>
           </Form.Row>
         </Card.Body>
         <Form.Row className="justify-content-center">
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" disabled>
             Update
           </Button>
         </Form.Row>
@@ -84,4 +151,4 @@ const AccEditForm = () => {
   );
 };
 
-export default AccEditForm;
+export default connect(mapStateToProps)(AccEditForm);
