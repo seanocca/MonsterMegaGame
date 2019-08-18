65d8188934cd360b56354322d8ba1d66c4b88fbb
squed <squed@squeds-iMac.lan>
2019-08-18 16:48:28 +1000

    Setup basis for beasts admin page

--- client/src/components/BeastsDashboard.jsx
+++ client/src/components/BeastsDashboard.jsx
@@ -1,54 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Table } from 'react-bootstrap';
+// TODO: use useDispatch functions for CRUD actions
+import { useSelector } from 'react-redux';
 import Dashboard from '../pages/Dashboard';
+import AdminBeast from './AdminBeast';
 
-class BeastsDashboard extends Component {
-  x = () => { /* Temp fix to remove list warnings */ };
-
-  render() {
-    return (
-      <Container style={{ padding: '4rem 0' }}>
-        <Dashboard />
-        <h1>Beasts</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Beast Name</th>
-              <th>Faction</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mitochondria</td>
-              <td>Mechanica</td>
-              <td>
-                <a href="/#">Edit Beast</a>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>Surveillance Team</td>
-              <td>Voidborn</td>
-              <td>
-                <a href="/#">Edit Beast</a>
-              </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Octothorpe</td>
-              <td>Biochondrys</td>
-              <td>
-                <a href="/#">Edit Beast</a>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-      </Container>
-    );
-  }
-}
+const BeastsDashboard = () => {
+  const beasts = useSelector((state) => state.beasts);
+  const beastComponents = beasts.map((factionBeasts) => (factionBeasts.beasts.map((beast) => 
+    <AdminBeast
+      name={beast.name}
+      desc={beast.desc}
+      move={beast.move}
+      damage={beast.damage}
+      maxdmg={beast.maxdmg}
+      leap={beast.leap}
+      maxleap={beast.maxleap}
+      hp={beast.hp}
+      maxhp={beast.maxhp}
+      speed={beast.speed}
+      maxspeed={beast.maxspeed}
+    />
+  )
+  ))
+  return (
+    <Container>
+    <Dashboard />
+      <h1>Beasts</h1>
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Desc</th>
+            <th>Move</th>
+            <th>Dmg</th>
+            <th>Max Dmg</th>
+            <th>Leap</th>
+            <th>Max Leap</th>
+            <th>HP</th>
+            <th>Max HP</th>
+            <th>Speed</th>
+            <th>Max Speed</th>
+          </tr>
+        </thead>
+      </Table>      
+      <div>
+        {beastComponents}
+      </div>
+    </Container>
+  );
+};
 
 export default BeastsDashboard;
