7778cc6b53cef5969e304dff96ce9715d2d5c2b1
squed <squed@squeds-iMac.lan>
2019-08-25 19:33:05 +1000

    process textarea text for faction admin

--- client/src/components/EditableFactionRow.jsx
+++ client/src/components/EditableFactionRow.jsx
@@ -5,10 +5,12 @@ import FactionRow from './FactionRow';
 const EditableFactionRow = (props) => {
   const [editFormOpen, setEditFormOpen] = useState(false);
   const { name, leader } = props;
+  const desc = props.desc.join('\n\n');
   if (editFormOpen) {
     return (
       <AdminFactionForm
         {...props}
+        desc={desc}
         onFormClose={() => setEditFormOpen(false)}
       />
     );
