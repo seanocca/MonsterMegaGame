767b73500d33f63a074c10c223cb33f246caf4ab
death667b <idaniel@spinnys.com>
2019-09-02 17:32:37 +1000

    ive got lint

--- client/src/pages/LoadData.jsx
+++ client/src/pages/LoadData.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import {
+  Container, Row, Col, Button,
+} from 'react-bootstrap';
 import { API } from 'aws-amplify';
 
 const Account = () => {
