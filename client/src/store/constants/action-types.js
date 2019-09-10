691d7d31d8fdb10129f013662195ef19b3f0542d
death667b <idaniel@spinnys.com>
2019-09-11 09:07:17 +1000

    new actions types (contants)

--- client/src/store/constants/action-types.js
+++ client/src/store/constants/action-types.js
@@ -6,16 +6,18 @@ export const GET_USER = 'GET_USER';
 export const SET_USER = 'SET_USER';
 export const PROCESS_USER = 'PROCESS_USER';
 // Factions
-export const DOWNLOAD_FACTIONS = 'DOWNLOAD_FACTIONS';
-export const PROCESS_DOWNLOAD_FACTIONS = 'PROCESS_DOWNLOAD_FACTIONS';
+export const DOWNLOAD_FACTIONS = 'faction';
+export const PROCESS_DOWNLOAD_FACTIONS = 'download_faction';
 export const CREATE_FACTION = 'CREATE_FACTION';
 export const EDIT_FACTION = 'EDIT_FACTION';
 // Beasts
-export const DOWNLOAD_BEASTS = 'DOWNLOAD_BEASTS';
+export const DOWNLOAD_BEASTS = 'beast';
+export const PROCESS_DOWNLOAD_BEASTS = 'download_beast';
 export const CREATE_BEAST = 'CREATE_BEAST';
 export const EDIT_BEAST = 'EDIT_BEAST';
 // Augments
 export const DOWNLOAD_AUGMENTS = 'DOWNLOAD_AUGMENTS';
+export const PROCESS_DOWNLOAD_AUGMENTS = 'DOWNLOAD_AUGMENTS';
 export const CREATE_AUGMENT = 'CREATE_AUGMENT';
 export const EDIT_AUGMENT = 'EDIT_AUGMENT';
 
