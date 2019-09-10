764cbea3f3cdac8eaca3051904e74555a3760950
death667b <idaniel@spinnys.com>
2019-09-11 09:06:45 +1000

    Trigger loadBeast redux action on page load

--- client/src/pages/Bestiary.jsx
+++ client/src/pages/Bestiary.jsx
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
-import BestiaryAccordion from '../components/BestiaryComponent'
 
-class Bestiary extends Component {
-  render() {
-    return(
-      <Container style={{ padding: '4rem 0' }}>
-        <BestiaryAccordion />
-      </Container>);
-  }
-}
+import { loadBeasts } from '../store/actions';
+import BestiaryAccordion from '../components/BestiaryComponent';
+
+const Bestiary = () => {
+  const dispatch = useDispatch();
+  dispatch(loadBeasts());
+
+  return (
+    <Container style={{ padding: '4rem 0' }}>
+      <BestiaryAccordion />
+    </Container>
+  );
+};
 
 export default Bestiary;
