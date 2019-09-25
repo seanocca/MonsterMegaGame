9c594230d6fcc36881ed7ca898608942635d8235
seanocca <32288543+seanocca@users.noreply.github.com>
2019-09-25 13:20:47 +1000

    Added How to Modal on Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { ButtonToolbar, Button, Container } from 'react-bootstrap';
 
 import Game from '../components/Game';
+import HowToModal from '../components/HowToModal';
 
 function Home() {
+  const [modalShow, setModalShow] = React.useState(false);
+
   return (
     <Container style={{ padding: '4rem 0', flex: '1 0 auto', minHeight: '100%' }}>
       <Game />
+      <ButtonToolbar>
+        <Button className="how-to-button" onClick={() => setModalShow(true)}>
+          How to Play!
+        </Button>
+
+        <HowToModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+      </ButtonToolbar>
     </Container>
   );
 }
