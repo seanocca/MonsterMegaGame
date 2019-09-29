734370d7c38ed669873f538f409b2533b62b3154
death667b <idaniel@spinnys.com>
2019-09-29 17:20:43 +1000

    Correctly use a table in react and HTML

--- client/src/components/Dashboard/UsersDashboard/EditableUserList/EditableUserRow/AdminUserForm/AdminUserForm.jsx
+++ client/src/components/Dashboard/UsersDashboard/EditableUserList/EditableUserRow/AdminUserForm/AdminUserForm.jsx
@@ -12,87 +12,88 @@ const AdminUserForm = (props) => {
   const state = useFormInput(props.state);
   const postcode = useFormInput(props.postcode);
 
-  const submitText = props.id ? 'Update' : 'Create';
   return (
-    <Accordion>
-      <Form style={{ padding: '1rem' }}>
-        <Form.Row>
-          <Form.Group as={Col} controlId="firstName">
-            <Form.Label>First Name</Form.Label>
+    <tr>
+      <td colSpan="3">
+        <Form style={{ padding: '1rem' }}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="firstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                placeholder="Firstname"
+                {...firstName}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="lastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                placeholder="Last Name"
+                {...lastName}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="current@email.com"
+                {...email}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Group controlId="address">
+            <Form.Label>Address</Form.Label>
             <Form.Control
-              placeholder="Firstname"
-              {...firstName}
+              placeholder="1234 Main St"
+              {...address}
             />
           </Form.Group>
-          <Form.Group as={Col} controlId="lastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control
-              placeholder="Last Name"
-              {...lastName}
-            />
-          </Form.Group>
-        </Form.Row>
-        <Form.Row>
-          <Form.Group as={Col} controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="current@email.com"
-              {...email}
-            />
-          </Form.Group>
-        </Form.Row>
-        <Form.Group controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            placeholder="1234 Main St"
-            {...address}
-          />
-        </Form.Group>
-        <Form.Row>
-          <Form.Group as={Col} controlId="city">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              placeholder="Brisbane"
-              {...city}
-            />
-          </Form.Group>
+          <Form.Row>
+            <Form.Group as={Col} controlId="city">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                placeholder="Brisbane"
+                {...city}
+              />
+            </Form.Group>
 
-          <Form.Group as={Col} controlId="state">
-            <Form.Label>State</Form.Label>
-            <Form.Control
-              as="select"
-              {...state}
-            >
-              <option value="">Choose...</option>
-              <option value="QLD">QLD</option>
-              <option value="NSW">NSW</option>
-              <option value="VIC">VIC</option>
-              <option value="NT">NT</option>
-              <option value="SA">SA</option>
-              <option value="WA">WA</option>
-              <option value="ACT">ACT</option>
-            </Form.Control>
-          </Form.Group>
+            <Form.Group as={Col} controlId="state">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                as="select"
+                {...state}
+              >
+                <option value="">Choose...</option>
+                <option value="QLD">QLD</option>
+                <option value="NSW">NSW</option>
+                <option value="VIC">VIC</option>
+                <option value="NT">NT</option>
+                <option value="SA">SA</option>
+                <option value="WA">WA</option>
+                <option value="ACT">ACT</option>
+              </Form.Control>
+            </Form.Group>
 
-          <Form.Group as={Col} controlId="postcode">
-            <Form.Label>Post Code</Form.Label>
-            <Form.Control
-              placeholder="1234"
-              {...postcode}
-            />
-          </Form.Group>
-        </Form.Row>
-        <Form.Row className="justify-content-center">
-          <Button variant="primary" type="submit">
-            {submitText}
-          </Button>
-          <Button variant="danger" type="button" onClick={onFormClose}>
-            Cancel
-          </Button>
-        </Form.Row>
-      </Form>
-    </Accordion>
+            <Form.Group as={Col} controlId="postcode">
+              <Form.Label>Post Code</Form.Label>
+              <Form.Control
+                placeholder="1234"
+                {...postcode}
+              />
+            </Form.Group>
+          </Form.Row>
+          <Form.Row className="justify-content-center">
+            <Button variant="primary" type="submit">
+              Update
+            </Button>
+            <Button variant="danger" type="button" onClick={onFormClose}>
+              Cancel
+            </Button>
+          </Form.Row>
+        </Form>
+      </td>
+    </tr>
   );
 };
 
