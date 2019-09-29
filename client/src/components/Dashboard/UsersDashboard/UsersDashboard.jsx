aacb56ddd8189b4014fcbb3b4020f091bd091dfc
death667b <idaniel@spinnys.com>
2019-09-29 16:32:50 +1000

    Load user list if not in redux yet

--- client/src/components/Dashboard/UsersDashboard/UsersDashboard.jsx
+++ client/src/components/Dashboard/UsersDashboard/UsersDashboard.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 
 import EditableUserList from './EditableUserList/EditableUserList';
 import Dashboard from '../Dashboard';
 
+import { getAllUsers } from '../../../store/actions';
+
 const UsersDashboard = () => {
+  const dispatch = useDispatch();
   const users = useSelector(state => state.users);
+
+  if (!users) dispatch(getAllUsers());
 
   return (
     <Container style={{ padding: '4rem 0' }}>
