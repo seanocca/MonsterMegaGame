b0f0b3c7116449a376f9a6ca5adebe121235006c
death667b <idaniel@spinnys.com>
2019-09-29 15:26:39 +1000

    Use LinkContainer to switch between dashboard routes

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Container, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Dashboard = () => (
   <Container>
     <Nav variant="tabs">
       <Nav.Item>
-        <Nav.Link href="/dashboard/users">Users</Nav.Link>
+        <LinkContainer to="/dashboard/users">
+          <Nav.Link>Users</Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/dashboard/beasts">Beasts</Nav.Link>
+        <LinkContainer to="/dashboard/beasts">
+          <Nav.Link>Beasts</Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/dashboard/augments">Augments</Nav.Link>
+        <LinkContainer to="/dashboard/augments">
+          <Nav.Link>Augments</Nav.Link>
+        </LinkContainer>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/dashboard/factions">Factions</Nav.Link>
+        <LinkContainer to="/dashboard/factions">
+          <Nav.Link>Factions</Nav.Link>
+        </LinkContainer>
       </Nav.Item>
     </Nav>
   </Container>
