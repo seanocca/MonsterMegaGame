bb0e1dfbd603408c08d4f9b01e1020a5a513a54a
death667b <idaniel@spinnys.com>
2019-10-07 12:02:43 +1000

    Hack to get the gatekeep logo to show

--- client/src/components/Dashboard/BeastsDashboard/EditableAllBeastsList/EditableFactionBeastsList/EditableBeastCard/BeastCard/BeastCard.jsx
+++ client/src/components/Dashboard/BeastsDashboard/EditableAllBeastsList/EditableFactionBeastsList/EditableBeastCard/BeastCard/BeastCard.jsx
@@ -11,14 +11,15 @@ const BeastCard = (props) => {
     maxleap, hp, maxhp, speed, maxspeed, onFormOpen,
   } = props;
   const factionStyles = useFactionCardStyles(faction);
-  const factionImagePath = `${factionImagePathPrefix + faction.toLowerCase()}.png`;
+  let factionImagePath = `${factionImagePathPrefix + faction.toLowerCase()}.png`;
+  if (faction === 'Gatekeepers') factionImagePath = `${factionImagePathPrefix}gatekeeper.png`;
   const altTag = `${faction} - ${name}`;
 
   return (
     <Card bg="dark" text="white" style={factionStyles}>
       <Card.Header>{faction} <img src={factionImagePath} alt={altTag} style={{ height: '1.3rem' }} /></Card.Header>
       {/* placeholder image */}
-      <Card.Img variant="top" src={image} style={{ height: '20rem' }} />
+      <Card.Img variant="top" src={image} style={{ height: '10rem' }} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle>{desc}</Card.Subtitle>
