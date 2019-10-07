7ea79bdb79d79014981b87901cddb48b1971e91f
death667b <idaniel@spinnys.com>
2019-10-07 10:00:37 +1000

    Display admin status

--- client/src/components/Dashboard/UsersDashboard/EditableUserList/EditableUserRow/UserRow/UserRow.jsx
+++ client/src/components/Dashboard/UsersDashboard/EditableUserList/EditableUserRow/UserRow/UserRow.jsx
@@ -3,13 +3,14 @@ import { Button } from 'react-bootstrap';
 
 const UserRow = (props) => {
   const {
-    firstName, lastName, onFormOpen,
+    firstName, lastName, onFormOpen, isAdmin,
   } = props;
   return (
     <tr>
       <td>{firstName}</td>
       <td>{lastName}</td>
       <td><Button type="button" onClick={onFormOpen}>Edit User</Button></td>
+      <td>{(isAdmin) ? 'Yes' : 'No'}</td>
     </tr>
   );
 };
