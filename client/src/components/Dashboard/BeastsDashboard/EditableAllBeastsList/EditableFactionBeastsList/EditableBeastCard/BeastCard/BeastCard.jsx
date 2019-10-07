3d6268940a017658ecc639eabbf573bb1a650e3d
death667b <idaniel@spinnys.com>
2019-10-07 11:30:14 +1000

    use correct prop name to display image

--- client/src/components/Dashboard/BeastsDashboard/EditableAllBeastsList/EditableFactionBeastsList/EditableBeastCard/BeastCard/BeastCard.jsx
+++ client/src/components/Dashboard/BeastsDashboard/EditableAllBeastsList/EditableFactionBeastsList/EditableBeastCard/BeastCard/BeastCard.jsx
@@ -7,7 +7,7 @@ const factionImagePathPrefix = '/img/factions/';
 
 const BeastCard = (props) => {
   const {
-    faction, name, desc, move, damage, maxdmg, leap, img,
+    faction, name, desc, move, damage, maxdmg, leap, image,
     maxleap, hp, maxhp, speed, maxspeed, onFormOpen,
   } = props;
   const factionStyles = useFactionCardStyles(faction);
@@ -18,7 +18,7 @@ const BeastCard = (props) => {
     <Card bg="dark" text="white" style={factionStyles}>
       <Card.Header>{faction} <img src={factionImagePath} alt={altTag} style={{ height: '1.3rem' }} /></Card.Header>
       {/* placeholder image */}
-      <Card.Img variant="top" src={img} style={{ height: '20rem' }} />
+      <Card.Img variant="top" src={image} style={{ height: '20rem' }} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle>{desc}</Card.Subtitle>
