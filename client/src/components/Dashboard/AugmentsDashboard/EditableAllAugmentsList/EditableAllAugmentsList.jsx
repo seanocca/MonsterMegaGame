5f17216fd76cd1ebffc08d0aa90558b7c5db46c4
seanocca <32288543+seanocca@users.noreply.github.com>
2019-11-05 12:27:43 +1000

    Fixed loading for dboard on aug and beast

--- client/src/components/Dashboard/AugmentsDashboard/EditableAllAugmentsList/EditableAllAugmentsList.jsx
+++ client/src/components/Dashboard/AugmentsDashboard/EditableAllAugmentsList/EditableAllAugmentsList.jsx
@@ -4,20 +4,24 @@ import EditableFactionAugmentsList from './EditableFactionAugmentsList';
 
 const EditableAllAugmentsList = (props) => {
   const { allAugments, onFormSubmit } = props;
-  const factionAugments = allAugments.map(factionAugments => (
-    <EditableFactionAugmentsList
-      faction={factionAugments.faction}
-      factionAugments={factionAugments.augments}
-      onFormSubmit={onFormSubmit}
-      key={factionAugments.faction}
-      logo={factionAugments.logo}
-    />
-  ));
+  if (allAugments) {
+    const factionAugments = allAugments.map(factionAugments => (
+      <EditableFactionAugmentsList
+        faction={factionAugments.faction}
+        factionAugments={factionAugments.augments}
+        onFormSubmit={onFormSubmit}
+        key={factionAugments.faction}
+        logo={factionAugments.logo}
+      />
+    ));
   return (
     <div>
       { factionAugments }
     </div>
   );
+  } else {
+    return <div />;
+  }
 };
 
 export default EditableAllAugmentsList;
--- client/src/components/Dashboard/AugmentsDashboard/EditableAllAugmentsList/EditableFactionAugmentsList/EditableFactionAugmentsList.jsx
+++ client/src/components/Dashboard/AugmentsDashboard/EditableAllAugmentsList/EditableFactionAugmentsList/EditableFactionAugmentsList.jsx
@@ -6,27 +6,37 @@ const EditableFactionAugmentsList = (props) => {
   const {
     faction, logo, factionAugments, onFormSubmit,
   } = props;
-  const factionAugmentsComponents = factionAugments.map(factionAugment => (
-    <EditableAugmentCard
-      {...factionAugment}
-      faction={faction}
-      onFormSubmit={onFormSubmit}
-      key={factionAugment.id}
-      logo={logo}
-    />
-  ));
-  return (
-    <Accordion>
-      <Accordion.Toggle as={Card.Header}>
-        <h2>{faction}</h2>
-      </Accordion.Toggle>
-      <Accordion.Collapse>
-        <Row>
-          {factionAugmentsComponents}
-        </Row>
-      </Accordion.Collapse>
-    </Accordion>
-  );
+  if (factionAugments) {
+    const factionAugmentsComponents = factionAugments.map(factionAugment => (
+      <EditableAugmentCard
+        {...factionAugment}
+        faction={faction}
+        onFormSubmit={onFormSubmit}
+        key={factionAugment.id}
+        logo={logo}
+      />
+    ));
+    return (
+      <Accordion>
+        <Accordion.Toggle as={Card.Header}>
+          <h2>{faction}</h2>
+        </Accordion.Toggle>
+        <Accordion.Collapse>
+          <Row>
+            {factionAugmentsComponents}
+          </Row>
+        </Accordion.Collapse>
+      </Accordion>
+    );
+  } else {
+    return (
+      <Accordion>
+        <Accordion.Toggle as={Card.Header}>
+          <h2>{faction}</h2>
+        </Accordion.Toggle>
+      </Accordion>
+    );
+  }
 };
 
 export default EditableFactionAugmentsList;
